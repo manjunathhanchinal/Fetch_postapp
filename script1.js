50f99626730b9839f3c2f99cd5d0038dddaefa01
manjunathhanchinal <141146202+manjunathhanchinal@users.noreply.github.com>
2024-07-16 09:01:14 +0530

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,9 +1,9 @@
 async function fetchPosts(){
    
     try{
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
         if(!response.ok) throw new Error('Network response was not ok');
-        const posts = await  response.json();
+        const posts = await response.json();
         displayPosts(posts);
     }catch(error){
         document.getElementById('posts-error').innerText = `Error: ${error.message}`
@@ -13,16 +13,21 @@ async function fetchPosts(){
 }
 
 
+
+
 function displayPosts(posts){
     const postsContainer = document.getElementById('posts');
     postsContainer.innerHTML = posts.map(post => `
         <div class='post'>
-           <input type="checkbox" ${post.completed ? 'checked': ''}>
-           <label>${post.title}</label>
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
         </div>
 
 
         `).join('');
 }
+
+
+
 
 fetchPosts();
